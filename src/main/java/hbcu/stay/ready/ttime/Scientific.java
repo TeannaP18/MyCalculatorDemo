80d8567f9c270c059f456be2cda748a1b71950ec
TeannaP18 <tannap905@gmail.com>
2019-07-24 10:11:33 -0400

    finished

---
+++ src/main/java/hbcu/stay/ready/ttime/Scientific.java
@@ -0,0 +1,12 @@
+package hbcu.stay.ready.ttime;
+
+public class ScientificCalculator extends Calculator {
+    private double memory;
+
+    public ScientificCalculator(){
+        memory = 0;
+    }
+    public double sine(){Math.sin()
+    }
+
+}
